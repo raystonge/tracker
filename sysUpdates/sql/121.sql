f2486b081b7db7b0a82d279140d00408d5857eec
Ray St. Onge <raymond.stonge@gmail.com>
2023-04-07 13:21:09 -0400

    Query to clean up some unused permissions

--- sysUpdates/sql/121.sql
+++ sysUpdates/sql/121.sql
@@ -1,1 +1,5 @@
 UPDATE `trackerVS`.`assetCondition` SET `showAll` = '0' WHERE (`assetConditionId` = '8');
+DELETE FROM `trackerVS`.`permission` WHERE (`permissionId` = '482');
+DELETE FROM `trackerVS`.`permission` WHERE (`permissionId` = '483');
+DELETE FROM `trackerVS`.`permission` WHERE (`permissionId` = '484');
+DELETE FROM `trackerVS`.`permission` WHERE (`permissionId` = '447');
